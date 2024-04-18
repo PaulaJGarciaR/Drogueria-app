1bb58a0fcb199d2789500ec915964b1b470a65f5
PaulaJGarciaR <pjgarciar@ufpso.edu.co>
2024-04-17 21:04:22 -0500

    modification of the product view index code

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -71,5 +71,43 @@
 </div>
 @endsection
 @push('script')
-
+<script type="text/javascript">
+		$(function () {
+			$("#example1").DataTable({
+				"responsive": true, 
+				"lengthChange": true, 
+				"autoWidth": false,
+				//"buttons": ["excel", "pdf", "print", "colvis"],
+				"language": 
+						{
+							"sLengthMenu": "Mostrar MENU entradas",
+							"sEmptyTable": "No hay datos disponibles en la tabla",
+							"sInfo": "Mostrando START a END de TOTAL entradas",
+							"sInfoEmpty": "Mostrando 0 a 0 de 0 entradas",
+							"sSearch": "Buscar:",
+							"sZeroRecords": "No se encontraron registros coincidentes en la tabla",
+							"sInfoFiltered": "(Filtrado de MAX entradas totales)",
+							"oPaginate": {
+								"sFirst": "Primero",
+								"sPrevious": "Anterior",
+								"sNext": "Siguiente",
+								"sLast": "Ultimo"
+							},
+							/*"buttons": {
+								"print": "Imprimir",
+								"colvis": "Visibilidad Columnas"
+								/*"create": "Nuevo",
+								"edit": "Cambiar",
+								"remove": "Borrar",
+								"copy": "Copiar",
+								"csv": "fichero CSV",
+								"excel": "tabla Excel",
+								"pdf": "documento PDF",
+								"collection": "Colección",
+								"upload": "Seleccione fichero...."
+							}*/
+						}
+			});//.buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+		});
+	</script>
 @endpush
